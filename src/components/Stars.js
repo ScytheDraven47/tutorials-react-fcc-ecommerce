08ff5495bc1ce9e18691805ba38cbc8778a7fbfb
ScytheDraven47 <ben.rogers.mckee@gmail.com>
2021-11-07 11:31:19 +1300

    stars functionality complete

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,22 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
+
+const Stars = ({ stars, reviews }) => {
+	const starIcons = Array.from({ length: 5 }, (_, index) => {
+		const fullStar = stars >= index + 1
+		const halfStar = stars >= index + 0.5
+		return (
+			<span key={index}>
+				{fullStar ? (
+					<BsStarFill />
+				) : halfStar + 0.5 ? (
+					<BsStarHalf />
+				) : (
+					<BsStar />
+				)}
+			</span>
+		)
+	})
+
+	return (
+		<Wrapper>
+			<div className='stars'>{starIcons}</div>
+			<p className='reviews'>({reviews} customer reviews)</p>
+		</Wrapper>
+	)
 }
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  span {
-    color: #ffb900;
-    font-size: 1rem;
-    margin-right: 0.25rem;
-  }
-  p {
-    margin-left: 0.5rem;
-    margin-bottom: 0;
-  }
-  margin-bottom: 0.5rem;
+	display: flex;
+	align-items: center;
+	span {
+		color: #ffb900;
+		font-size: 1rem;
+		margin-right: 0.25rem;
+	}
+	p {
+		margin-left: 0.5rem;
+		margin-bottom: 0;
+	}
+	margin-bottom: 0.5rem;
 `
 export default Stars
