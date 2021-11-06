3f072812978344e3ef4ff4bc89a7e7201b43441a
ScytheDraven47 <ben.rogers.mckee@gmail.com>
2021-11-06 18:39:58 +1300

    fixed price for featured items

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,8 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+	return Intl.NumberFormat('en-NZ', {
+		style: 'currency',
+		currency: 'NZD',
+	}).format(number / 100)
+}
 
 export const getUniqueValues = () => {}
